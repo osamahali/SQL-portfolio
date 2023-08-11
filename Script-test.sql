2a33c9fbfb79710b6413112491a2df2f4ebb6164
Osamah Alzahrani <47139366+osamahali@users.noreply.github.com>
2023-08-11 19:16:45 +0300

    upload
    test upload

---
+++ Script-test.sql
@@ -0,0 +1,11 @@
+# How to find 
+
+INSERT INTO suppliers (name, email, phone)
+VALUES ('Galactic Gear', 'contact@galacticgear.com', '123-456-7890');
+
+select * from suppliers s 
+
+
+UPDATE suppliers
+SET email = 'wow@hotmail.com'
+WHERE name = 'Interstellar Itineraries';
